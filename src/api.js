4074fe08c92c37a3920663e5dcaeeccc7bb708be
Felipe <fpizarro.ux@gmail.com>
2022-08-29 20:37:34 +0200

    first commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4fbc7221bemsh5345f21568e04d4p124195jsn28a62309fcdd',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
